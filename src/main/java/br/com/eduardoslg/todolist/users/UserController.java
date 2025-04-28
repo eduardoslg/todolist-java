fce12fc41f79279ae2c546c14645796f3d6ab757
Eduardo Lage <eduardo@atmsistema.com.br>
2025-04-28 18:38:21 -0300

    feat: rota de listagem de usuários

--- src/main/java/br/com/eduardoslg/todolist/users/UserController.java
+++ src/main/java/br/com/eduardoslg/todolist/users/UserController.java
@@ -3,8 +3,13 @@ package br.com.eduardoslg.todolist.users;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/v1/users")
@@ -19,4 +24,13 @@ public class UserController {
 
         return output;
     }
+
+    @GetMapping("")
+    public List<UserModel> list() {
+
+        List<UserModel> output = this.userRepository.findAll();
+
+        return output;
+    }
+    
 }
